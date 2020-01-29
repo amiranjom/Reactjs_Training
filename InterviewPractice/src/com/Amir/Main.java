e9ca0c01715df0b98f6c8d83d6dfc5405da1c59e
Amir <ar.anjomshoaa@gmail.com>
2020-01-29 15:47:42 -0800

    Java TwoSum

---
+++ InterviewPractice/src/com/Amir/Main.java
@@ -0,0 +1,12 @@
+package com.Amir;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TwoSum testCase = new TwoSum();
+        int[] nums = {3,2,4};
+        testCase.twoSum(nums,6);
+        //--------------------------------------------------------------
+
+    }
+}
